af3a6a82d527a388d73dc9c2b5eaf7cff4288742
GodzFeivur <godzfeivvur@gmail.com>
2021-11-24 19:45:36 +0100

    funtionality

--- details.js
+++ details.js
@@ -1,20 +1,88 @@
-fetch('https://restcountries.com/v2/alpha')
-    .then((res) => res.json())
-    .then((data) => {
-        let outputs = ''
-        data.forEach(function(country) {
-            outputs += `
-        <div class="boxed shadow-lg w-60 h-74 rounded-lg">
-            <img src="${country.flag}" class="w-60 h-32" alt="">
-            <div class="px-6 pb-6">
-            <a href="details.html"><h2 class="font-bold py-4">${country.name}</h2> </a>
-                <ul>
-                    <li><span class="font-semibold">Population:</span> ${country.population}</li>
-                        <li><span class="font-semibold">Region:</span>${country.region}</li> 
-                    <li><span class="font-semibold">Capital:</span> ${country.capital}</li> 
-                </ul>
-            </div>
-        </div>`
-        });
-        document.querySelector(".outputs").innerHTML = outputs
-    });
+let output = document.querySelector('.output')
+let dataArray = []
+output.innerHTML = ""
+const singlepage = () => {
+    const urlID = window.location.search;
+    fetch('https://restcountries.com/v2/alpha')
+        .then(res => res.json())
+        .then(data => {
+            data.array.forEach(country => {
+                output.innerHTML += `
+                <div class="boxed country-card cursor-pointer shadow-lg w-60 h-74 rounded-lg">
+                <img src="${country.flags.svg}" class="w-60 h-32" alt="">
+                <div class="px-6 pb-6">
+                <h2 class="country-name font-bold py-4">${country.name.official}</h2>
+                    <ul>
+                        <li><span class="font-semibold">Population:</span> ${country.population}</li>
+                            <li><span class="font-semibold">Region:</span>${country.region}</li> 
+                        <li class="search-id"><span class="font-semibold">Capital:</span> ${country.capital}</li> 
+                    </ul>
+                </div>
+            </div>`
+            });
+        })
+
+    .catch(error => {
+        output.innerHTML = `<div class="">${error.message}</div>`
+    })
+}
+singlepage();
+const getPosts = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => {
+            dataArray = data
+            renderPosts(data);
+            filterPost()
+        })
+        .catch(error => {
+            output.innerHTML = `<div>${error.message}</div>`
+        })
+}
+getPosts()
+
+const renderPosts = (post) => {
+    let output = post.map((country) => {
+            return `
+        <div class="boxed country-card cursor-pointer shadow-lg w-60 h-74 rounded-lg">
+        <a href="details.html?capital=${country.capital}">
+        <img src="${country.flags.svg}" class="w-60 h-32" alt="">
+        <div class="px-6 pb-6">
+        <h2 class="search-title font-bold py-4">${country.name.official}</h2>
+            <ul>
+                <li><span class="font-semibold">Population:</span> ${country.population}</li>
+                    <li><span class="font-semibold">Region:</span>${country.region}</li> 
+                <li class="search-id"><span class="font-semibold">Capital:</span> ${country.capital}</li> 
+            </ul>
+        </div>
+        </a>
+    </div>
+        `
+        })
+        .join('')
+    document.querySelector(".output").innerHTML = output
+}
+
+let alertplaceholder = document.getElementById('liveAlertPlaceholder')
+
+function alerts(message) {
+    let wrapper = document.createElement('div')
+    wrapper.innerHTML = `<div class="">'+ message +'<button class="btn btn-primary"></button></div>`
+    alertplaceholder.append(wrapper)
+}
+const filterPost = () => {
+    let search = document.getElementById('search-box')
+    let searchHide = document.querySelectorAll('.boxed')
+    search.addEventListener('keyup', (e) => {
+        const term = e.target.value.toLowerCase()
+        searchHide.forEach((item) => {
+            let title = item.querySelector('.search-title').innerText;
+            let id = item.querySelector('.search-id').innerText;
+            if (title.toLowerCase().indexOf(term) != -1 || id.indexOf(term)) {
+                item.style.display = 'block'
+            } else {
+                item.style.display = 'none'
+            }
+        })
+    })
+}
--- script.js
+++ script.js
@@ -7,26 +7,26 @@ document.querySelector('.dark').addEventListener('click', function() {
     document.querySelector('.header').classList.toggle('navActive');
 });
 
-fetch('https://restcountries.com/v3.1/all')
-    .then((res) => res.json())
-    .then((data) => {
-        let output = ''
-        data.forEach(function(country) {
-            output += `
-        <div class="boxed country-card cursor-pointer shadow-lg w-60 h-74 rounded-lg" onclick="name()">
-            <img src="${country.flags.svg}" class="w-60 h-32" alt="">
-            <div class="px-6 pb-6">
-            <h2 class="country-name font-bold py-4">${country.name.official}</h2>
-                <ul>
-                    <li><span class="font-semibold">Population:</span> ${country.population}</li>
-                        <li><span class="font-semibold">Region:</span>${country.region}</li> 
-                    <li><span class="font-semibold">Capital:</span> ${country.capital}</li> 
-                </ul>
-            </div>
-        </div>`
-        });
-        document.querySelector(".output").innerHTML = output
-    });
+// fetch('https://restcountries.com/v3.1/all')
+//     .then((res) => res.json())
+//     .then((data) => {
+//         let output = ''
+//         data.forEach(function(country) {
+//             output += `
+//         <div class="boxed country-card cursor-pointer shadow-lg w-60 h-74 rounded-lg">
+//             <img src="${country.flags.svg}" class="w-60 h-32" alt="">
+//             <div class="px-6 pb-6">
+//             <h2 class="country-name font-bold py-4">${country.name.official}</h2>
+//                 <ul>
+//                     <li><span class="font-semibold">Population:</span> ${country.population}</li>
+//                         <li><span class="font-semibold">Region:</span>${country.region}</li> 
+//                     <li><span class="font-semibold">Capital:</span> ${country.capital}</li> 
+//                 </ul>
+//             </div>
+//         </div>`
+//         });
+//         document.querySelector(".output").innerHTML = output
+//     });
 
 
 document.getElementById('select').addEventListener("change", function() {
@@ -64,7 +64,7 @@ document.getElementById('select').addEventListener("change", function() {
         <div class="boxed shadow-lg w-60 h-80 rounded-lg">
             <img src="${country.flags.png}" class="w-60 h-32" alt="">
             <div class="px-6 pb-6">
-              <h2 class="font-bold py-4">${country.name.official}</h2> 
+              <h2 class="font-bold py-4">${country.name.official}q</h2> 
                 <ul>
                     <li><span class="font-semibold">Population:</span> ${country.population}</li>
                         <li><span class="font-semibold">Region:</span> ${country.region}</li> 
@@ -149,55 +149,3 @@ document.getElementById('select').addEventListener("change", function() {
             });
     }
 });
-document.getElementById('input').addEventListener("keyup", function() {
-    fetch('https://restcountries.com/v2/all')
-        .then((res) => res.json())
-        .then((data) => {
-            let input = document.getElementById('input')
-            inputVal = input.value
-            data.forEach(function(country) {
-                scvc
-            });
-        });
-});
-document.querySelector('.boxed').addEventListener('click', function() {
-    fetch('https://restcountries.com/v2/all')
-        .then((res) => res.json())
-        .then((data) => {
-            let output = ''
-            data.forEach(function(country) {
-                output += `
-        <div class="boxed shadow-lg w-60 h-74 rounded-lg">
-        <a href="details.html">country</a>
-            <img src="${country.flag}" class="w-60 h-32" alt="">
-            <div class="px-6 pb-6">
-              <h2 class="font-bold py-4">${country.name}</h2> 
-                <ul>
-                    <li><span class="font-semibold">Population:</span> ${country.population}</li>
-                        <li><span class="font-semibold">Region:</span>${country.region}</li> 
-                    <li><span class="font-semibold">Capital:</span> ${country.capital}</li> 
-                </ul>
-            </div>
-        </div>`
-            });
-            document.querySelector(".output").innerHTML = output
-        });
-});
-// const searchInput = document.querySelector(".input");
-// // search input
-// searchInput.addEventListener("keyup", (e) => {
-//     const { value } = e.target;
-
-//     // console.log(countryCard)
-//     const countryName = document.querySelectorAll(".country-name");
-//     console.log(countryName)
-
-//     countryName.forEach((name) => {
-//         if (name.textContent.toLowerCase().includes(value.toLowerCase())) {
-//             name.closest(".country-card").style.display = "block";
-//             alert('f')
-//         } else {
-//             name.closest(".country-card").style.display = "none";
-//         }
-//     });
-// });
